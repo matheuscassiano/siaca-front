be9d21583ce5d6b209d110f51ab4f629351419f0
Matheus Cassiano <matheuscassiano9@gmail.com>
2023-08-09 18:36:10 -0300

    feat: add large button component

---
+++ styles/LargeButton.ts
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import { styled } from "styled-components";
+
+const LargeButton = styled(Button)`
+    width: 100%;
+    padding: .8em 1em;
+`;
+
+export default LargeButton;
